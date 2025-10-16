e58cf5998b80eecd8f1cba0d845b75df8dee25c5
Gobbledyglomp <skoobidyboo@pm.me>
2025-10-16 20:39:06 +0200

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,7 @@ let persons = [
 morgan.token('body', req => JSON.stringify(req.body))
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
